83c1b20b0a59c1820a51175edbbb0437fad3164b
sam199510 <273045049@qq.com>
2018-03-12 15:55:47 +0800

    update

--- src/main/java/com/sam/graduation/design/gdmusicserver/service/special/SpecialService.java
+++ src/main/java/com/sam/graduation/design/gdmusicserver/service/special/SpecialService.java
@@ -14,6 +14,5 @@ public interface SpecialService {
     List<SpecialDto> findLikeSpecialName(String specialName);
 
     List<ArtistSpecialDto> findASLikeSpecialName(String specialName);
-
-
+    
 }
